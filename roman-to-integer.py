6ef8fb72ce9baa6133b50b93535f3d0407d01cf1
crispin <changmin@redice-inc.com>
2020-01-05 20:38:54 +0900

    roman-to-integer

---
+++ roman-to-integer.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+class Solution:
+
+    ROMAN_MAP = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    def romanToInt(self, s: str) -> int:
+        idx = 0
+        result = 0
+        while idx < len(s) - 1:
+            v1 = self.ROMAN_MAP[s[idx]]
+            v2 = self.ROMAN_MAP[s[idx + 1]]
+            if v1 >= v2:
+                result += v1
+                idx += 1
+            else:
+                result += (v2 - v1)
+                idx += 2
+        if idx == len(s) - 1:
+            result += self.ROMAN_MAP[s[-1]]
+        return result
+
+
+if __name__ == '__main__':
+    for roman in (
+        'III', # 3
+        'IV', # 4
+        'IX', # 9
+        'LVIII', # 58
+        'MCMXCIV', # 1994
+    ):
+        print(Solution().romanToInt(roman))
